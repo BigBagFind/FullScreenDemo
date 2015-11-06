a9535822624d60876d43596bc46514e8419df897
BigBagFind <594228924@qq.com>
2015-11-06 23:18:56 +0800

    Full

---
+++ FullScreenDemo/NavViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavViewController.h
+//  FullScreenDemo
+//
+//  Created by 吴玉铁 on 15/11/6.
+//  Copyright © 2015年 铁哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavViewController : UINavigationController
+
+@end
